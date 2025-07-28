ebfce451e19458ef22c8526302f5e0a44f7d4d79
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-07-28 12:23:44 +0200

    Add alternative/simple dp solution to problem 31

--- pe031.py
+++ pe031.py
@@ -10,5 +10,15 @@ def num_ways(n, coins):
     return dp[len(coins)][n]
 
 
+def num_ways_2(n, coins):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for coin in coins:
+        for j in range(coin, n + 1):
+            dp[j] += dp[j - coin]
+    return dp[n]
+
+
 if __name__ == "__main__":
     print(num_ways(200, [1, 2, 5, 10, 20, 50, 100, 200]))
+    print(num_ways_2(200, [1, 2, 5, 10, 20, 50, 100, 200]))
