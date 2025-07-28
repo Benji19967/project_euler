02ecb5537becd955967742e6b5b6790bfaf02ffb
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-07-28 14:41:39 +0200

    Add solution to problem 35

---
+++ pe035.py
@@ -0,0 +1,40 @@
+import sympy
+
+
+def digits_of_int(n):
+    digits = []
+    while n > 0:
+        digits.append(n % 10)
+        n //= 10
+    return reversed(digits)
+
+
+def get_rotations(n):
+    num_digits = len(str(n))
+    num_rotations = num_digits - 1
+    rotations = []
+    for _ in range(num_rotations):
+        d = n % 10
+        n //= 10
+        n = n + d * 10 ** (num_digits - 1)
+        rotations.append(n)
+    return rotations
+
+
+def main():
+    primes = []
+    for n in range(1_000_001):
+        if sympy.isprime(n):
+            primes.append(n)
+
+    count = 0
+    for p in primes:
+        rotations = get_rotations(p)
+        if not rotations or all(sympy.isprime(r) for r in rotations):
+            count += 1
+            print(p, rotations)
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
